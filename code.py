41a0366dde54449824b867613e030487fa47751f
Raymond Pang <124433610+pppkkkkkkk@users.noreply.github.com>
2025-09-28 06:02:18 +0000

    Fixed

--- code.py
+++ code.py
@@ -5,9 +5,20 @@ class Solution:
         
         for i in range(len(flowerbed)):
 
-            if flowerbed[i]==0 and (flowerbed[i-1] ==0 or i==0) and (flowerbed[i+1]==0 or i==len(flowerbed)-1):
+            if flowerbed[i]==0 and (i==0 or flowerbed[i-1] ==0) and (i==len(flowerbed)-1 or flowerbed[i+1]==0):
                 flowerbed[i] = 1
                 n-=1
                 if n == 0:
                     return True
         return False
+
+    # def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+    #     if n == 0:
+    #         return True
+    #     for i in range(len(flowerbed)):
+    #         if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == len(flowerbed)-1 or flowerbed[i+1] == 0):
+    #             flowerbed[i] = 1
+    #             n -= 1
+    #             if n == 0:
+    #                 return True
+    #     return False
